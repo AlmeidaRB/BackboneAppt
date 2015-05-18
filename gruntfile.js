daa6011ca165320594d99a9e283372b8c4dda092
Rachel Almeida <rachel.almeida@benefitfocus.com>
2015-05-18 15:22:04 -0400

    finalizing environment setup

---
+++ gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  // Configure task(s)
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: 'src/js/*.js', //source javascript files
+        dest: 'js/script.min.js' //destination, output file
+      },
+      dev: {
+        options: {
+          beautify: true,
+          mangle: false,
+          compress: false,
+          preserveComments: 'all'
+        },
+        src: 'src/js/*.js',
+        dest: 'js/script.min.js'
+      }
+    },
+    sass: {
+      dev: {
+        options: {
+          outputStyle: 'expanded'
+        },
+        files: {
+          'css/styles.css' : 'src/scss/styles.scss'
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: ['src/js/*.js'], //when these files change
+        tasks: ['uglify:dev'] //run this task
+      }
+    }
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+
+  // Register task(s)
+  grunt.registerTask('default', ['uglify:dev', 'sass:dev']);
+  grunt.registerTask('build', ['uglify:build']);
+
+};
